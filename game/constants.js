4757fc23bc9d9e6458c13da8504d59e3f3095f97
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-12-20 21:21:27 +0100

    feat: :sparkles: add keyInput from player

--- game/constants.js
+++ game/constants.js
@@ -9,5 +9,9 @@ const primary = "#8890EC"
 const secondary = "#591898"
 const accent = "#be4ae2"
 
-canvas = document.getElementById("canvas")
-ctx = canvas.getContext("2d")
+const body = document.querySelector("body")
+
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+//What direction player is walking
+let direction = null
--- game/main.js
+++ game/main.js
@@ -2,3 +2,28 @@
 Functional code where the logic lies
 */
 gameBoard.draw()
+
+body.addEventListener("keydown", registerKeypress)
+
+
+function registerKeypress(e) {
+
+    if (e.keyCode == 38) {
+        direction="up"
+    }
+    else if (e.keyCode == 39) {
+        console.log("RIGHT");
+        direction="right"
+    }
+    else if (e.keyCode == 37) {
+        console.log("LEFT");
+        direction="left"
+    }
+    else if (e.keyCode == 40) {
+        console.log("DOWN");
+        direction="down"
+    }
+    else {
+        console.log(e.keyCode);
+    }
+}
